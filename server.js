518161b35b91e4b256c427c8fbf56ac3c7dab05d
Dan Aument <danaument@gmail.com>
2021-03-02 11:28:59 -0600

    minor change to server.js log on start

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-tracker"
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on localhost:${PORT}!`);
 });
